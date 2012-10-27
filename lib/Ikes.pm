51ce11e55523fcaab369b64157df61b58231f221
ytnobody / satoshi azuma <ytnobody@gmail.com>
2012-10-27 17:19:27 +0900

    initial commit

---
+++ lib/Ikes.pm
@@ -0,0 +1,32 @@
+package Ikes;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Ikes -
+
+=head1 SYNOPSIS
+
+  use Ikes;
+
+=head1 DESCRIPTION
+
+Ikes is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
